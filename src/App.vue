4a542dbfa4dcd8137f1305d0cbf6c74414ae245b
macha <likeacompleteboss@gmail.com>
2023-07-24 17:36:57 +0800

    invert images!!!!

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
   <div class="content-container">
     <section class="section-container" id="missions" style="width:435px; height:714px;">
       <div class="section-header clipped-medium-backward">
-        <img src="/icons/mission-icon.svg" />
+        <img class = "less" src="/icons/mission-icon.svg" />
         <h1>Mission Log</h1>
       </div>
       <div class="section-content-container">
@@ -23,7 +23,11 @@
     </section>
     <section class="section-container" id="events" style="width:435px; height:714px;">
       <div class="section-header clipped-medium-backward">
-        <img src="/icons/events-icon.svg" />
+		<style>
+		.less {
+			filter: invert(100%);
+		}
+        <img class="less" src="/icons/events-icon.svg" />
         <h1>Events Log</h1>
       </div>
       <div class="section-content-container">
@@ -33,7 +37,7 @@
     <section class="section-container" id="pilots" style="width:894px; height:714px;">
       <div style="height:52px; overflow:hidden;">
         <div class="section-header clipped-medium-backward-pilot">
-          <img src="/icons/pilot-icon.svg" />
+          <img class="less" src="/icons/pilot-icon.svg" />
           <h1>Pilot Roster</h1>
         </div>
         <div class="rhombus-back">&nbsp;</div>
